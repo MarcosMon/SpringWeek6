9a7ca1e939de5db56388f37d104e946450695726
Marcos Monjon Mas <sysmarc13@hotmail.com>
2019-05-26 19:14:54 +0200

    Tercer caso test pasado

--- 12_setmana_3_exercici/src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ 12_setmana_3_exercici/src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -6,13 +6,14 @@ import java.util.List;
 import org.formacio.domain.Factura;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 
 
 public interface FacturesRepositori extends Repository<Factura, Long> {
 	
-	@Query("select count(*) from Client ")
-	public Number totalClient(String client); 	
+	@Query("select sum(linia.total) from Factura factura join factura.linies linia where factura.client.nom = :client")
+	public Number totalClient(@Param("client")String client); 	
 	
 	public List<Factura> findByClientNom(String nom);
 }
